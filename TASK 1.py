54a1dd453db28ff0174d70d401a439d1261d2435
Pranava Prakash J <pran.hackphysics@gmail.com>
2025-06-09 00:04:18 +0530

    Update TASK 1.py

--- TASK 1.py
+++ TASK 1.py
@@ -1,15 +1,16 @@
 def factorial(num):
-    if num < 0:
-        return 0
-    elif num < 2:
+    if num < 2:
         return 1
     else:
         return num * (factorial(num-1))
 
-number = int(input("Enter a number: "))
-fact = factorial(number)
+try:
+    number = int(input("Enter a number: "))
 
-if fact == 0:
-    print("Factorial cannot be negative")
-else:
-    print("Factorial of", number, "is:", fact)
+    if number < 0:
+        print("Factorial cannot be negative")
+    else:
+        print("Factorial of", number, "is:", factorial(number))
+
+except ValueError:
+    print("Number not valid")
